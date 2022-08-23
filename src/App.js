60627efaa55fce612dd6baad17a39a8c105a9bf4
Bruno Orozco <orozco.bruno@comunidad.unam.mx>
2022-08-23 11:12:19 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
-function App() {
+const url="https://brunomejia.pythonanywhere.com/api/restaurants/";
+
+class App extends Component {
+
+  state={
+    data:[],
+  }
+
+  peticionGet=()=>{
+    axios.get(url).then(response=>{
+      this.setState({data: response.data});
+    }).catch(error=>{
+      console.log(error.message);
+    })
+  }
+
+  componentDidMount() {
+    this.peticionGet();
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button className="btn btn-success">Agregar restaurante</button>
+        <table className="table ">
+          <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>País</th>
+            <th>Capital Bursatil (en millones de USD)</th>
+            <th>Acciones</th>
+          </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map(restaurant=>{
+                return(
+                  <tr>
+                    <td>{restaurant.id}</td>
+                    <td>{restaurant.restaurant_name}</td>
+                    <td>{restaurant.restaurant_type}</td>
+                    <td>{restaurant.address}</td>
+                    <td></td>
+                  </tr>
+                )
+            })}
+
+          </tbody>
+        </table>
     </div>
   );
+}
 }
 
 export default App;
